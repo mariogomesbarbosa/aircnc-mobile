69ac2fcfc180c7ec544182b4cbd5e1514a32c702
Mário Barbosa <mariogomes@mbp-de-mario.matriz.procenge.com.br>
2019-10-07 13:58:48 -0300

    Features: List(Finishing)

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {  AsyncStorage, StyleSheet, SafeAreaView, Text, Image } from 'react-native'
+
+import SpotList from '../components/SpotList'
+
+import logo from '../assets/logo.png'
 
 export default function List() {
+  const [techs, setTechs] = useState([])
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim())
+
+      setTechs(techsArray)
+    })
+  }, [])
+
   return (
-    <View />
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  
-});
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 20,
+  },
+})
