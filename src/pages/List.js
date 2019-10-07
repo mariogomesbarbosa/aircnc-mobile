16c431faf78406a21e2f1371c3ae790183fd21f5
Mário Barbosa <mariogomes@MacBook-Pro-de-Mario.local>
2019-10-07 20:59:05 -0300

    Features: Logout Button (Finish)

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import {  AsyncStorage, StyleSheet, SafeAreaView, Text, Image } from 'react-native'
+import {  AsyncStorage, StyleSheet, SafeAreaView, Text, Image, TouchableOpacity } from 'react-native'
 
 import SpotList from '../components/SpotList'
+import api from '../services/api'
 
 import logo from '../assets/logo.png'
 
-export default function List() {
+export default function List({ navigation }) {
   const [techs, setTechs] = useState([])
+  const [email] = useState('')
+
+  async function handleLogout() {
+    const response = await api.post('/sessions', {
+      email
+    })
+
+    const {_id } = response.data
+
+    await AsyncStorage.removeItem('user')
+    await AsyncStorage.removeItem('techs') 
+
+    navigation.navigate('Login')
+  }
 
   useEffect(() => {
     AsyncStorage.getItem('techs').then(storagedTechs => {
@@ -21,6 +36,9 @@ export default function List() {
       <Image style={styles.logo} source={logo} />
 
       {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+      <TouchableOpacity onPress={handleLogout} style={styles.button}>
+        <Text style={styles.buttonText}>Logout</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
@@ -35,5 +53,21 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
     alignSelf: "center",
     marginTop: 20,
+  },
+
+  button: {
+    height: 52,
+    backgroundColor: '#7259C1',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    marginHorizontal: 10,
+    marginTop: 20,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 })
